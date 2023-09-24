a22153860f21b4c2164dfd165c6714bdab6e419a
Cutter Crane <clgcrane@uab.edu>
2023-09-24 17:25:09 -0500

    tenatively done

--- user.cpp
+++ user.cpp
@@ -1,20 +1,32 @@
 #include "user.h"
 
-void User::addFriend(User *)
+void User::addFriend(User * p)
 {
-    
+    friendList.push_back(p);
 }
 
 void User::removeFriend(std::string user)
 {
+    //find the friend to remove
+    int index = 0;
+    for (User* p:friendList){
+
+        if(p->userName == user) p = nullptr;
+        index++;
+    }
+    //shifts vector left, replacing the friend to be removed
+    for(int i = index; i < friendList.size()-1; i++ ){
+        friendList.at(i) = friendList.at(i+1);
+    }
 }
 
 int User::numFriends()
 {
-    return 0;
+
+    return friendList.size();
 }
 
 User *User::getFriendAt(int n)
 {
-    return nullptr;
+    return friendList.at(n);
 }
